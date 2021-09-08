4e69b0417d8ff35f3b50712994e28ff798e58ca5
Faith Hidalgo <minepremium013@gmail.com>
2021-09-08 08:30:12 -0700

    * section 4 (Profile API Routes)

--- models/Profile.js
+++ models/Profile.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 
+mongoose.connect(uri, { useFindAndModify: false });
 const ProfileSchema = new mongoose.Schema({
     user: {
         type: mongoose.Schema.Types.ObjectId,
